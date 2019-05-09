c8c971ef005612825fc9f16eed415b9ee5622512
zhepingLiu <zheping.ProvidenceO@gmail.com>
2019-05-09 21:17:20 +1000

    fix jump and next_to

--- part-B-skeleton-1.1/best_AI_ever/game_board.py
+++ part-B-skeleton-1.1/best_AI_ever/game_board.py
@@ -19,19 +19,13 @@ class GameBoard:
     Output: True if they are next to each other, otherwise False
     """
     def next_to(self, position_1, position_2):
+        HEX_STEPS = [(-1, 0), (0, -1), (1, -1), (1, 0), (0, 1), (-1, 1)]
         (q1, r1) = position_1
         (q2, r2) = position_2
 
-        if q1 == q2 and abs(r1 - r2) == 1:
-            # same column case
-            return True
-        elif r1 == r2 and abs(q1 - q2) == 1:
-            # same row case
-            return True
-        elif abs(r1 - r2) == 1 and abs(q1 - q2) == 1 and \
-                (r1 - r2) + (q1 - q2) == 0:
-            # third case, e.g. (0, -3) and (-1, -2)
-            return True
+        for step_q, step_r in HEX_STEPS:
+            if (q1 + step_q, r1 + step_r) == (q2, r2):
+                return True
 
         return False
 
@@ -58,6 +52,14 @@ class GameBoard:
         elif r1 == r2 and q1 - q2 == -1:
             q = q2 + 1
             r = r1
+        elif q1 - q2 == -1 and r1 - r2 == 1:
+            # e.g. (-1,0) and (0,-1)
+            q = q1 + 2
+            r = r1 - 2
+        elif r1 - r2 == -1 and q1 - q2 == 1:
+            # e.g. (2,1) and (1,2)
+            q = q1 - 2
+            r = r1 + 2
 
         # check if the generated position is in the valid range
         if (q, r) in self.get_game_board():
